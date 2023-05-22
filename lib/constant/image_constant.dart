920c79589cf7d7d2904a8122359f8ce979ef34dc
Rama Agus <rama.agus@majoo.id>
2023-05-23 02:34:49 +0700

    add image constant

---
+++ lib/constant/image_constant.dart
@@ -0,0 +1,28 @@
+class ImageConstant {
+  static const aries = 'assets/zodiacs/aries.png';
+  static const taurus = 'assets/zodiacs/taurus.png';
+  static const gemini = 'assets/zodiacs/gemini.png';
+  static const cancer = 'assets/zodiacs/cancer.png';
+  static const leo = 'assets/zodiacs/leo.png';
+  static const virgo = 'assets/zodiacs/virgo.png';
+  static const libra = 'assets/zodiacs/libra.png';
+  static const scorpio = 'assets/zodiacs/scorpio.png';
+  static const sagitarius = 'assets/zodiacs/sagitarius.png';
+  static const capricorn = 'assets/zodiacs/capricorn.png';
+  static const aquarius = 'assets/zodiacs/aquarius.png';
+  static const pisces = 'assets/zodiacs/pisces.png';
+
+  //chinese zodiac sign
+  static const rat = 'assets/zodiacs/rat.png';
+  static const ox = 'assets/zodiacs/ox.png';
+  static const tiger = 'assets/zodiacs/tiger.png';
+  static const rabbit = 'assets/zodiacs/rabbit.png';
+  static const dragon = 'assets/zodiacs/dragon.png';
+  static const snake = 'assets/zodiacs/snake.png';
+  static const horse = 'assets/zodiacs/horse.png';
+  static const goat = 'assets/zodiacs/goat.png';
+  static const monkey = 'assets/zodiacs/monkey.png';
+  static const rooster = 'assets/zodiacs/rooster.png';
+  static const dog = 'assets/zodiacs/dog.png';
+  static const pig = 'assets/zodiacs/pig.png';
+}
